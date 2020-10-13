09a1263dda1d546d948469c40a69c7ad657f9cdb
Tomasz Wroclawski <t.wroclawski@amiya-group.eu>
2020-10-13 18:55:01 +0200

    0.8

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,20 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Tomasz";
+        int myAge = 48;
+        double height = 1.78;
+        char nameClass ='F';
+        boolean isGoodFather = true;
+        int myShoe = 43;
+        String myHebrewName = "Teomo";
+        boolean isGoodMother = false;
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(nameClass);
+        System.out.println(isGoodFather);
+        System.out.println(myShoe);
+        System.out.println(myHebrewName);
+        System.out.println(isGoodMother);
+    }
+}
