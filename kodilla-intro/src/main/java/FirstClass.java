431be2f1f836256b442b42d0d267188eaa5e685f
Tomasz Wroclawski <t.wroclawski@amiya-group.eu>
2020-10-18 14:09:35 +0200

    0.11 instrukcje warunkowe

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,5 +1,10 @@
 public class FirstClass {                           // [1]
-    public static void main(String[] args) {         // [2]
-        System.out.println("Hello from FirstClass!"); // [3]
+    public static void main(String[] args) {
+        System.out.println("Hello from FirstClass");
+        if (1 > 2) {
+            System.out.println("Niesamowite ! Jeden jest większe niż dwa !!!");
+        } else {
+            System.out.println("Wszystko po staremu, jeden jest mniejsze niż dwa.");
+        }
     }
 }
---
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -0,0 +1,21 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        int year = 1940;
+        System.out.println(year);
+        {
+            if (year%4!=0){
+                System.out.println("Rok nie przestępny");
+            } else if (year%100!=0){
+                System.out.println("Rok przestępny");
+            } else if (year%400!=0){
+                System.out.println("Rok nie przestępny");
+            } else {
+                System.out.println("Rok przestępny");
+            }
+        }
+    }
+}
+
+
+
+
