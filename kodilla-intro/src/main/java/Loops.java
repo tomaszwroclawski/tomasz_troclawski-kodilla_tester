ce31fcbb0264fa93cab8984c1683f0b199c32e16
Tomasz Wroclawski <t.wroclawski@amiya-group.eu>
2020-10-19 20:06:24 +0200

    Loops i suma elementów

--- kodilla-intro/src/main/java/Loops.java
+++ kodilla-intro/src/main/java/Loops.java
@@ -6,11 +6,16 @@ public class Loops {
         number [2] = 3;
         number [3] = 4;
         number [4] = 5;
+        int i = 0;
+        while (i <= 10  ) {
+            System.out.println(i);
+            i++;
+        }
         int numberOfElements = number.length;
         System.out.println(numberOfElements + "-ilość elementów");
         System.out.println("Moja tablica zawiera " + numberOfElements + " elementów");
+        System.out.println("Suma elementów tablicy to: "+ sumNumbers(number));
         }
-
     public static int sumNumbers (int[] number) {
         int result = 0;
         for(int i = 0; i < number.length; i++) {
