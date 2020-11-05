435b8a9ab42413e008f2321288fdcb65f6ab0e76
Tomasz Wroclawski <t.wroclawski@amiya-group.eu>
2020-11-05 14:31:49 +0100

    2.3

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,10 +1,49 @@
-public class FirstClass {                           // [1]
+public class FirstClass {
+
     public static void main(String[] args) {
-        System.out.println("Hello from FirstClass");
-        if (1 > 2) {
-            System.out.println("Niesamowite ! Jeden jest większe niż dwa !!!");
-        } else {
-            System.out.println("Wszystko po staremu, jeden jest mniejsze niż dwa.");
-        }
+        Notebook notebook = new Notebook("600g",1000,2020);
+        System.out.println(notebook.weight);
+        System.out.println(notebook.price);
+        System.out.println(notebook.year);
+        notebook.checkPrice();
+        notebook.checkYear();
+        notebook.checkWeight();
+
+
+
+
+
+
+        Notebook heavyNotebook = new Notebook("2000g",1500,2000);
+        System.out.println(heavyNotebook.weight);
+        System.out.println(heavyNotebook.price);
+        System.out.println(heavyNotebook.year);
+        heavyNotebook.checkPrice();
+        heavyNotebook.checkYear();
+        heavyNotebook.checkWeight();
+
+
+
+
+        Notebook oldNotebook = new Notebook("1600g",500, 2004);
+        System.out.println(oldNotebook.weight);
+        System.out.println(oldNotebook.price);
+        System.out.println(oldNotebook.year);
+        oldNotebook.checkPrice();
+        oldNotebook.checkYear();
+        oldNotebook.checkWeight();
+
+        System.out.println(notebook.weight+" "+notebook.price+" "+notebook.year);
+        System.out.println(heavyNotebook.weight+" "+heavyNotebook.price+" "+heavyNotebook.year);
+        System.out.println(oldNotebook.weight+" "+oldNotebook.price+" "+oldNotebook.year);
+
+
+        System.out.println("To jest oferta dla Ciebie :");
+
+        notebook.checkYourMoney();
+        heavyNotebook.checkYourMoney();
+        oldNotebook.checkYourMoney();
+
+
     }
 }
---
+++ kodilla-intro/src/main/java/Notebook.java
@@ -0,0 +1,112 @@
+public class Notebook {
+    String weight;
+    int price;
+    int checkPrice;
+    int checkYear;
+    int year;
+    int checkYourMoney;
+    int money;
+
+
+    public Notebook(String weight, int price, int year) {
+        this.weight = weight;
+        this.price = price;
+        this.year = year;
+        this.money=money;
+
+
+    }
+
+    public void checkPrice() {
+        if (this.price < 600) {
+            System.out.println("This notebook is very cheap.");
+        } else if (this.price < 1000) {
+            System.out.println("This price is good.");
+        } else if (this.price > 1001) {
+            System.out.println("This notebook is expensive.");
+        } else {
+            System.out.println("This price is good.");
+        }
+    }
+
+    public void checkYear() {
+        if (this.year <= 2000) {
+            System.out.println("To jest rocznik 2000");
+        } else if (this.year <= 2004) {
+            System.out.println("To jest rocznik 2004");
+        } else if (this.year >= 2020) {
+            System.out.println("To jest rocznik 2020");
+        } else {
+            System.out.println("To jest rocznik 2000");
+        }
+
+
+    }
+
+    public void checkWeight() {
+        if (convert(this.weight) <= 600) {
+            System.out.println("to jest waga poniżej lub równa 600g");
+        } else if (convert(this.weight) <= 1600) {
+            System.out.println("To jest waga 1600g");
+        } else if
+        (convert(this.weight) >= 1601) {
+            System.out.println("To jest waga pow 1600g");
+        } else {
+            System.out.println("to jest waga poniżej lub równa  600g");
+
+
+        }
+    }
+
+    private Integer convert(String imput) {
+        String newString = imput.replace("g", "").replace("G", "");
+        Integer newInteger = Integer.valueOf(newString);
+        return newInteger;
+    }
+
+
+    public void checkYourMoney() {
+        int money = 1000;
+
+
+        System.out.println("To jest twój budżet:" + "  " + money + "  zł");
+
+        if (money >= 500) {
+            System.out.println("Zwraca wartość Notebook za  " + this.price + " zł, wyprodukowany w  " + this.year);
+
+
+        }
+        else if (money >= 1000)
+        {
+            System.out.println("Zwraca wartość Notebook za   " + this.price + " zł, wyprodukowany w  " + this.year);
+
+        }
+        else if (money >= 1500)
+        {
+            System.out.println("Zwraca wartość Notebook za   " + this.price + " zł, wyprodukowany w  " + this.year);
+        }
+        else if (money < 500)
+        {
+            System.out.println("Musisz wygospodarować minimum 500 zł");
+        }
+
+        String budget = money <= 500 ? "kupisz za tą kasę oldNotebook": "  ";
+        String budget1 = money >= 1000 ? "kupisz za tą kasę Notebook lub 2 oldNotebook":"  ";
+        String budget2 = money >= 1500 ? "kupisz za tą kasę newNotebook lub 3 oldNotebooki lub 1oldNotebook i 1Notebook minimum i zostanie ci trochę reszty": "  ";
+        System.out.println(budget);
+        System.out.println(budget1);
+        System.out.println(budget2);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
