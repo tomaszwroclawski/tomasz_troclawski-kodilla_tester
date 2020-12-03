b96ffffe3517bbdbce611d2313ed455a736dbf75
Tomasz Wroclawski <t.wroclawski@amiya-group.eu>
2020-12-03 12:02:27 +0100

    Revert "3.5 rozbudowa_testów_kalkulatora"
    This reverts commit 0f6ab1ba

--- kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
@@ -9,10 +9,4 @@ public class Calculator {
     public int subtract(int a, int b) {
         return (a - b);
     }
-
-    public double power(double c, double n) {
-        double y = Math.pow(c,n);
-        return y;
-
-    }
 }
