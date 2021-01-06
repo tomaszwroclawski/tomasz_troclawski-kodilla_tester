af8087869fda308ac1f4fc26108bac6e18b1842c
Tomasz Wroclawski <t.wroclawski@amiya-group.eu>
2021-01-06 21:48:12 +0100

    5.5 DictionaryTestSuite

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutble/special/ImmutableSpecialCases.java
@@ -0,0 +1,34 @@
+package com.kodilla.collections.adv.immutble.special;
+
+import java.util.logging.SocketHandler;
+
+public class ImmutableSpecialCases {
+    public static void main(String[] args) {
+        Integer a= 128;
+        Integer b = 128;
+        System.out.println(a==b);
+        System.out.println(a.equals(b));
+        System.out.println("---------------------");
+        String x="This is string";
+        String y = "This is string";
+        System.out.println(x==y);
+        System.out.println(y.equals(x));
+        System.out.println("---------------------");
+        Integer c = 100;
+        Integer d = 100;
+        System.out.println(c.hashCode());
+        System.out.println(d.hashCode());
+        System.out.println(c==d);
+        c=c+10;
+        System.out.println(c.hashCode());
+        System.out.println(d.hashCode());
+        System.out.println(c==d);
+        c=10567;
+        d=10567;
+        System.out.println(c.hashCode());
+        System.out.println(d.hashCode());
+        System.out.println(c==d);
+
+
+    }
+}
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/TaskManager.java
@@ -0,0 +1,15 @@
+package com.kodilla.stream.homework;
+
+public class TaskManager {
+    public static void main(String[] args) {
+      /*  List<Task> deadlines = TaskRepository.getTasks()
+                .stream()
+                .filter(task -> task.getDeadline().isAfter(LocalDate.now()))
+//                .map(task -> task.getDeadline())
+                .collect(Collectors.toList());
+        deadlines.forEach(System.out::println);  //deadlines.forEach(e -> System.out.println(e));
+
+    }
+    }*/
+    }
+}
