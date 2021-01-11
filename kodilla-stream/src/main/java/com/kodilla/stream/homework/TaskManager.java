800245a025a7299c2a9061ab769b627bfa1a0980
Tomasz Wroclawski <t.wroclawski@amiya-group.eu>
2021-01-11 22:15:24 +0100

    6.1 Lista zadań (stream)

--- kodilla-stream/src/main/java/com/kodilla/stream/homework/TaskManager.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/TaskManager.java
@@ -1,15 +1,16 @@
 package com.kodilla.stream.homework;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class TaskManager {
     public static void main(String[] args) {
-      /*  List<Task> deadlines = TaskRepository.getTasks()
+        List<LocalDate> deadlines = TaskRepository.getTasks()
                 .stream()
                 .filter(task -> task.getDeadline().isAfter(LocalDate.now()))
-//                .map(task -> task.getDeadline())
+                .map(task -> task.getDeadline())
                 .collect(Collectors.toList());
-        deadlines.forEach(System.out::println);  //deadlines.forEach(e -> System.out.println(e));
-
-    }
-    }*/
+        System.out.println(deadlines);
     }
 }
--- kodilla-stream/src/main/java/com/kodilla/stream/homework/TaskRepository.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/TaskRepository.java
@@ -1,10 +1,13 @@
 package com.kodilla.stream.homework;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TaskRepository {
-    /*public static List<Task> getTasks(){
+    public static List<Task> getTasks(){
         List<Task> tasks = new ArrayList<>();
         tasks.add(new Task("Zapoznać się z dokumentacją", LocalDate.of(2020, Month.JANUARY, 15), LocalDate.of(2020, Month.APRIL, 10)));
         tasks.add(new Task("Napisać testy", LocalDate.of(2010, Month.FEBRUARY, 21), LocalDate.of(2020, Month.MAY, 28)));
@@ -12,5 +15,6 @@ public class TaskRepository {
         tasks.add(new Task("Przetestować aplikację", LocalDate.of(2020, Month.JUNE, 15), LocalDate.of(2021, Month.SEPTEMBER, 17)));
         tasks.add(new Task("Napisać raport", LocalDate.of(2010, Month.SEPTEMBER, 1), LocalDate.of(2021, Month.OCTOBER, 28)));
         return tasks;
-    }*/
+    }
+
 }
