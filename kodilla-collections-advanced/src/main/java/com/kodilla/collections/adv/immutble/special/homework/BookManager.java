869c5e078bcd53856a7f13f1cd46f2dd0c4ca08f
Tomasz Wroclawski <t.wroclawski@amiya-group.eu>
2021-01-11 17:56:56 +0100

    5.4 Księgozbiór - z poprawkami do zatwierdzenia

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutble/special/homework/BookManager.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutble/special/homework/BookManager.java
@@ -7,10 +7,16 @@ public class BookManager {
     static Set<Book> books = new HashSet<>();
 
 
-    public static Book createBook (String title,String author){
+    public static Book createBook(String title, String author) {
         Book book = new Book(title, author);
         books.add(book);
+
+        for (Book book1 : books) {
+            if (book1.equals(book)) {
+                return book1;
+            }
+
+        }
         return book;
     }
-
 }
